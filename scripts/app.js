db6a3ff369d68a5c91ddd60f51b7d97e53077ca9
Steven Maguire <stevenmaguire@gmail.com>
2017-04-05 20:54:41 -0500

    Add lazy loading images

--- scripts/app.js
+++ scripts/app.js
@@ -482,3 +482,15 @@
         });
     }
 })(Isotope);
+
+(function (window) {
+    function lazyLoadImages() {
+        var imgDefer = document.getElementsByTagName('img');
+        for (var i=0; i<imgDefer.length; i++) {
+            if (imgDefer[i].getAttribute('data-src')) {
+                imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+            }
+        }
+    }
+    window.onload = lazyLoadImages;
+})(window);
